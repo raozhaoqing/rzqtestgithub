92e5bb004aed4df184934d8e24ecf307bf4496f7
raozhaoqing <rzq@raozhaoqingdeMacBook-Pro.local>
2017-05-12 14:27:44 +0800

    这是这次提交的内容

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log(123456);
+const http = require('http')
+
+const homePage = `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset='utf-8'>
+		<title>
+			raozhaoqing
+		</title>
+	</head>
+		<body>
+			<h1>我使用nodejs建的一个服务<h1>
+			<h2>mangdb</h2>
+		</body>
+	</html>
+
+`
+
+http.createServer((req,res) => {
+	res.statusCode = 200
+	res.setHeader('content-Type','text/html')
+	res.end(homePage)
+}).
+listen(3000,() => {
+	console.log("server Running at 3000")
+})
